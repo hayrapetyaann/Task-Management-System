27a8923fdf1938e8c6101c8f5c94b84d3bd339ef
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-02 22:43:52 +0400

    Fix Priority and Status issues

--- Task.cpp
+++ Task.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 Task::Task(int task_id, int uid, std::string title, std::string description, std::string deadline,
-           std::string category, int prio, int status)
+           std::string category, Priority prio, Status status)
     : m_task_id(task_id), m_uid(uid), m_title(title), m_description(description), m_deadline(deadline),
       m_category(category), m_prio(prio), m_status(status) {}
 
@@ -56,16 +56,17 @@ void Task::edit(const Task& updated_task) {
 }
 
 void Task::set_status(Status status) {
-    m_status = static_cast<int>(status);
+    m_status = status;
 }
 
 void Task::set_prio(Priority prio) {
-    m_prio = static_cast<int>(prio);
+    m_prio = prio;
 }
 
 void Task::display_info() const {
     std::cout << "Task ID: " << m_task_id << ", Title: " << m_title << ", Deadline: " << m_deadline
-              << ", Category: " << m_category << ", Priority: " << m_prio << ", Status: " << m_status << std::endl;
+              << ", Category: " << m_category << ", Priority: " << static_cast<int>(m_prio)
+              << ", Status: " << static_cast<int>(m_status) << std::endl;
 }
 
 int Task::get_task_id() const {
@@ -73,34 +74,31 @@ int Task::get_task_id() const {
 }
 
 Task& Task::operator++() {
-    if (m_status < 2) {
-        m_status++;
+    if (m_status != Status::COMPLETED) {
+        m_status = static_cast<Status>(static_cast<int>(m_status) + 1);
     }
     return *this;
 }
 
 Task Task::operator++(int) {
     Task temp = *this;
-    if (m_status < 2) {
-        m_status++;
-    }
+    ++(*this);
     return temp;
 }
 
 Task& Task::operator--() {
-    if (m_status > 0) {
-        m_status--;
+    if (m_status != Status::NOT_STARTED) {
+        m_status = static_cast<Status>(static_cast<int>(m_status) - 1);
     }
     return *this;
 }
 
 Task Task::operator--(int) {
     Task temp = *this;
-    if (m_status > 0) {
-        m_status--;
-    }
+    --(*this);
     return temp;
 }
+
 
 bool Task::operator==(const Task& other) const {
     return m_task_id == other.m_task_id;
@@ -120,18 +118,23 @@ bool Task::operator>(const Task& other) const {
 
 std::ostream& operator<<(std::ostream& os, const Task& task) {
     os << "Task ID: " << task.m_task_id << ", Title: " << task.m_title << ", Deadline: " << task.m_deadline
-       << ", Category: " << task.m_category << ", Priority: " << task.m_prio << ", Status: " << task.m_status;
+       << ", Category: " << task.m_category << ", Priority: " << static_cast<int>(task.m_prio) << ", Status: " << static_cast<int>(task.m_status);
   
     return os;
 }
 
 std::istream& operator>>(std::istream& is, Task& task) {
-    is >> task.m_task_id;
+    int prio, status;
+    is >> task.m_task_id >> task.m_uid;
+    is.ignore();
     std::getline(is, task.m_title);
     std::getline(is, task.m_description);
     std::getline(is, task.m_deadline);
     std::getline(is, task.m_category);
-    is >> task.m_prio;
-    is >> task.m_status;
+    is >> prio >> status;
+
+    task.m_prio = static_cast<Priority>(prio);
+    task.m_status = static_cast<Status>(status);
+    
     return is;
 }
