9d358ac3833bea9cd26f43c26e7a5804545a29ab
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-02 23:07:40 +0400

    FIx m_task_id and m_uid issues, and fix memory leak in delete_task

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -1,5 +1,6 @@
 #include "TaskManager.h"
 #include <iostream>
+#include <algorithm>
 
 TaskManager::TaskManager() {}
 
@@ -33,13 +34,18 @@ void TaskManager::add_task(Task* task) {
     m_all_tasks.push_back(task);
 }
 
-void TaskManager::delete_task(const std::string& task_id) {
-    m_all_tasks.erase(std::remove_if(m_all_tasks.begin(), m_all_tasks.end(),
-                                      [&](Task* task) { return task->get_task_id() == task_id; }),
-                       m_all_tasks.end());
+void TaskManager::delete_task(const int task_id) {
+    for (auto it = m_all_tasks.begin(); it != m_all_tasks.end();) {
+        if ((*it)->get_task_id() == task_id) {
+            delete *it;  
+            it = m_all_tasks.erase(it); 
+        } else {
+            ++it;  
+        }
+    }
 }
 
-void TaskManager::edit_task(const std::string& task_id, const Task& updated_task) {
+void TaskManager::edit_task(const int task_id, const Task& updated_task) {
     for (auto task : m_all_tasks) {
         if (task->get_task_id() == task_id) {
             *task = updated_task;
@@ -48,7 +54,7 @@ void TaskManager::edit_task(const std::string& task_id, const Task& updated_task
     }
 }
 
-void TaskManager::display_task(const std::string& task_id) const {
+void TaskManager::display_task(const int task_id) const {
     for (auto task : m_all_tasks) {
         if (task->get_task_id() == task_id) {
             std::cout << *task << std::endl;
